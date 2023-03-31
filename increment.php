47b1c40d1b1e5697efd0d3d1b40b104ce5567136
KCkingcollin <34804902+KCkingcollin@users.noreply.github.com>
2023-03-30 23:48:47 -0400

    Update increment.php

--- increment.php
+++ increment.php
@@ -1,33 +1,30 @@
 <?php
+
 // Define constants
 define('FILE_PATH', 'count.txt');
 define('MAX_COUNT', 10);
 
-// Read the "global count" value from the file and store it in memory
-$globalCount = (int) file_get_contents(FILE_PATH);
+// Initialize global count value in memory
+$globalCount = intval(file_get_contents(FILE_PATH));
 
-// Check if the "count" parameter was passed
+// Check if count parameter was passed
 if (isset($_POST['count'])) {
-    $count = (int) $_POST['count'];
-    
-    // If the "count" value is within the maximum limit, update the "global count" value in memory
-    if ($count <= MAX_COUNT) {
+    $count = intval($_POST['count']);
+    // Check if count is within max count and not 0
+    if ($count > 0 && $count <= MAX_COUNT) {
+        // Add count value to global count in memory
         $globalCount += $count;
     }
 }
 
-// Update the file and "global count" value every 5 seconds
-$lastModifiedTime = filemtime(FILE_PATH);
-while (true) {
-    if ($globalCount != (int) file_get_contents(FILE_PATH)) {
-        $difference = $globalCount - (int) file_get_contents(FILE_PATH);
-        file_put_contents(FILE_PATH, $globalCount);
-        flock($file, LOCK_UN);
-    }
-    sleep(5);
-    clearstatcache();
-    if (filemtime(FILE_PATH) > $lastModifiedTime) {
-        $globalCount = (int) file_get_contents(FILE_PATH);
-        $lastModifiedTime = filemtime(FILE_PATH);
-    }
+// Echo global count
+echo $globalCount;
+
+// Update file if global count has changed
+if ($globalCount !== intval(file_get_contents(FILE_PATH))) {
+    $difference = $globalCount - intval(file_get_contents(FILE_PATH));
+    file_put_contents(FILE_PATH, $globalCount);
+    $globalCount = intval(file_get_contents(FILE_PATH));
 }
+
+?>
